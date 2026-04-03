64c45356cfb0525430664da3ee869dc30319a94b
itorly <itorly@163.com>
2026-04-03 23:31:16 +0800

    extract specific details

--- Iterative.py
+++ Iterative.py
@@ -73,3 +73,52 @@ Technical specifications: ```{fact_sheet_chair}```
 """
 response = get_completion(prompt)
 print(response)
+
+# extract specific details
+# The description is intended for furniture retailers, 
+# so should be technical in nature and focus on the 
+# materials the product is constructed from.
+prompt = f"""
+Your task is to help a marketing team create a 
+description for a retail website of a product based 
+on a technical fact sheet.
+
+Write a product description based on the information 
+provided in the technical specifications delimited by 
+triple backticks.
+
+The description is intended for furniture retailers, 
+so should be technical in nature and focus on the 
+materials the product is constructed from.
+
+Use at most 50 words.
+
+Technical specifications: ```{fact_sheet_chair}```
+"""
+response = get_completion(prompt)
+print(response) 
+
+# At the end of the description, include every 7-character 
+# Product ID in the technical specification.
+prompt = f"""
+Your task is to help a marketing team create a 
+description for a retail website of a product based 
+on a technical fact sheet.
+
+Write a product description based on the information 
+provided in the technical specifications delimited by 
+triple backticks.
+
+The description is intended for furniture retailers, 
+so should be technical in nature and focus on the 
+materials the product is constructed from.
+
+At the end of the description, include every 7-character 
+Product ID in the technical specification.
+
+Use at most 50 words.
+
+Technical specifications: ```{fact_sheet_chair}```
+"""
+response = get_completion(prompt)
+print(response) 
