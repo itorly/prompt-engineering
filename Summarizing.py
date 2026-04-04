2e2680cace0d5142c155ad72bab6af94b5fd6c63
itorly <itorly@163.com>
2026-04-05 00:03:12 +0800

    4.1 Generate a summary of the comments

--- Summarizing.py
+++ Summarizing.py
@@ -14,3 +14,29 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
  temperature=0, # this is the degree of randomness of the model's output
  )
  return response.choices[0].message["content"]
+
+prod_review = """
+Got this panda plush toy for my daughter's birthday, \
+who loves it and takes it everywhere. It's soft and \ 
+super cute, and its face has a friendly look. It's \ 
+a bit small for what I paid though. I think there \ 
+might be other options that are bigger for the \ 
+same price. It arrived a day earlier than expected, \ 
+so I got to play with it myself before I gave it \ 
+to her.
+""" 
+
+# 4.1.Generate a summary of the comments
+
+prompt = f"""
+Your task is to generate a short summary of a product \
+review from an ecommerce site. 
+
+Summarize the review below, delimited by triple 
+backticks, in at most 30 words. 
+
+Review: ```{prod_review}```
+"""
+
+response = get_completion(prompt)
+print(response)
