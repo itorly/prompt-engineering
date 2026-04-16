6b61bd81edff6293b6af25e09c5768559f8bd8a5
itorly <itorly@163.com>
2026-04-17 00:01:51 +0800

    5.4.Integrate multiple tasks

--- Inferring.py
+++ Inferring.py
@@ -89,3 +89,32 @@ print(response)
 #  "Item": "lamp",
 #  "Brand": "Lumina"
 # } 
+
+
+## 5.4.Integrate multiple tasks
+prompt = f"""
+Identify the following items from the review text: 
+- Sentiment (positive or negative)
+- Is the reviewer expressing anger? (true or false)
+- Item purchased by reviewer
+- Company that made the item
+
+The review is delimited with triple backticks. \
+Format your response as a JSON object with \
+"Sentiment", "Anger", "Item" and "Brand" as the keys.
+If the information isn't present, use "unknown" \
+as the value.
+Make your response as short as possible.
+Format the Anger value as a boolean.
+
+Review text: '''{lamp_review}'''
+"""
+response = get_completion(prompt)
+print(response) 
+
+# {
+#  "Sentiment": "positive",
+#  "Anger": false,
+#  "Item": "lamp with additional storage",
+#  "Brand": "Lumina"
+# }
