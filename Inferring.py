06d167cdacb7dfa591d385bb4e8f9fd47d913531
itorly <itorly@163.com>
2026-04-20 00:00:35 +0800

    5.6.Text Topic Index

--- Inferring.py
+++ Inferring.py
@@ -160,3 +160,23 @@ Text sample: '''{story}'''
 """
 response = get_completion(prompt)
 print(response) 
+
+## 5.6.Text Topic Index
+topic_list = [
+ "nasa", "local government", "engineering", 
+ "employee satisfaction", "federal government"
+] 
+
+prompt = f"""
+Determine whether each item in the following list of \
+topics is a topic in the text below, which
+is delimited with triple backticks.
+
+Give your answer as list with 0 or 1 for each topic.\
+
+List of topics: {", ".join(topic_list)}
+
+Text sample: '''{story}'''
+"""
+response = get_completion(prompt)
+print(response) 
